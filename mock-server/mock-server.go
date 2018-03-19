a9bb1fb762164b7ede49fff762de271824085855
Chandan Kumar <chandan.kr404@gmail.com>
2018-03-19 15:31:21 +0530

    Added topology inside persistentvolume tab (#28)
    -Modified mock-server.go for adding `persistentvolume`
    `persistentvolumeclaim` `storageclass` `application pod`
    
    -Added pv.json file that contains details about the topology shown
    inside `PERSISTENT VOLUME` tab.
    
    Signed-off-by: Chandan <chandan.kr404@gmail.com>

--- mock-server/mock-server.go
+++ mock-server/mock-server.go
@@ -4,8 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
+	"time"
 )
 
 type apiData struct {
@@ -63,12 +67,72 @@ type Topology struct {
 	URL string `json:"url"`
 }
 
+type persistentvolume struct {
+	Add    interface{} `json:"add"`
+	Update []struct {
+		ID         string `json:"id"`
+		Label      string `json:"label"`
+		LabelMinor string `json:"labelMinor"`
+		Rank       string `json:"rank"`
+		Shape      string `json:"shape"`
+		Metadata   []struct {
+			ID       string  `json:"id"`
+			Label    string  `json:"label"`
+			Value    string  `json:"value"`
+			Priority float64 `json:"priority"`
+			DataType string  `json:"dataType,omitempty"`
+		} `json:"metadata"`
+		Metrics []struct {
+			ID       string      `json:"id"`
+			Label    string      `json:"label"`
+			Format   string      `json:"format,omitempty"`
+			Value    float64     `json:"value"`
+			Priority float64     `json:"priority"`
+			Samples  interface{} `json:"samples"`
+			Min      float64     `json:"min"`
+			Max      float64     `json:"max"`
+			First    time.Time   `json:"first"`
+			Last     time.Time   `json:"last"`
+			URL      string      `json:"url"`
+			Group    string      `json:"group,omitempty"`
+		} `json:"metrics"`
+		Adjacency []string `json:"adjacency"`
+	} `json:"update"`
+	Remove interface{} `json:"remove"`
+}
+
 func main() {
 	router := gin.Default()
+	router.GET("/api/topology/persistentVolume/ws", func(c *gin.Context) {
+		wshandler(c.Writer, c.Request)
+	})
 	router.GET("/api", api)
 	router.GET("/api/topology", topology)
 	router.Run(":4040")
 
+}
+
+// ------ websocket -------------------
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func wshandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for {
+		raw, _ := ioutil.ReadFile("json/pv.json")
+		var z persistentvolume
+		json.Unmarshal(raw, &z)
+		conn.WriteJSON(z)
+		return
+	}
 }
 
 /*-------------------------------------------------------*/
