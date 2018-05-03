3401eb9c2ec222007a8754add8057b204bbd2221
Satyam Zode <satyam.zode@openebs.io>
2018-05-03 15:23:21 +0530

    Fix adjacency in the render
    Signed-off-by: Satyam Zode <satyam.zode@openebs.io>

--- render/filters.go
+++ render/filters.go
@@ -80,10 +80,10 @@ func (f FilterFunc) Transform(nodes Nodes) Nodes {
 				newAdjacency = newAdjacency.Add(dstID)
 			}
 		}
-		if kubernetes.VolumeClaim == "" {
+		claimName, ok := node.Latest.Lookup(kubernetes.VolumeClaim)
+		if claimName == "" || !ok {
 			node.Adjacency = newAdjacency
 		}
-
 		output[id] = node
 	}
 
